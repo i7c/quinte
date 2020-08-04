0bf555ca1b72c3d3b21cd3f66ca284c130ba3925
Constantin Weißer <i7c@posteo.de>
2020-08-04 20:41:56 +0200

    Add bindings for libnotmuch

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .whitelist_function("notmuch_database_open")
+        .whitelist_function("notmuch_database_destroy")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    bindings
+        .write_to_file("bindings.rs")
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:rustc-link-lib=notmuch");
+}
