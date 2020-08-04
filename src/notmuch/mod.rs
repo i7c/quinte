64b60b6ee9dd53506d6e810f818344fc7d524f8f
Constantin Weißer <i7c@posteo.de>
2020-08-05 00:18:39 +0200

    Simplify handling of notmuch error messages

--- src/notmuch/mod.rs
+++ src/notmuch/mod.rs
@@ -11,14 +11,8 @@ use std::os::raw;
 use std::ptr;
 
 #[derive(Debug)]
-pub struct NotmuchErrorDetail {
-    pub message: String,
-    pub original_error: notmuch_status_t,
-}
-
-#[derive(Debug)]
 pub enum NotmuchError {
-    DbFailedToOpen(NotmuchErrorDetail),
+    DbFailedToOpen(String),
 }
 
 pub type NotmuchResult<T> = std::result::Result<T, NotmuchError>;
@@ -28,38 +22,38 @@ pub struct NotmuchDb {
     db_ptr: *mut notmuch_database_t,
 }
 
+fn convert_error_message(ptr: *mut raw::c_char) -> String {
+    unsafe {
+        if !ptr.is_null() {
+            let message = CStr::from_ptr(ptr)
+                .to_str()
+                .unwrap_or_else(|_| "error message could not be decoded")
+                .to_owned();
+            libc::free(ptr as *mut libc::c_void);
+            return message;
+        } else {
+            "no detailled error message".to_owned()
+        }
+    }
+}
+
 impl NotmuchDb {
     pub fn open(path: &str) -> Result<NotmuchDb, NotmuchError> {
-        let mut db_ptr: *mut notmuch_database_t = ptr::null_mut();
         let path = CString::new(path).expect("Failed to create CString");
 
         let result: notmuch_status_t;
-        let message: Option<String>;
+        let mut db_ptr: *mut notmuch_database_t = ptr::null_mut();
+        let mut msg: *mut raw::c_char = ptr::null_mut();
         unsafe {
-            let mut msg: *mut raw::c_char = ptr::null_mut();
             result = notmuch_database_open_verbose(
                 path.as_ptr(),
                 notmuch_database_mode_t_NOTMUCH_DATABASE_MODE_READ_ONLY,
                 &mut db_ptr,
                 &mut msg,
             );
-            if !msg.is_null() {
-                message = Some(
-                    CStr::from_ptr(msg)
-                        .to_str()
-                        .expect("libnotmuch error message was not valid utf8")
-                        .to_owned(),
-                );
-                libc::free(msg as *mut libc::c_void);
-            } else {
-                message = None;
-            }
         }
         if result != _notmuch_status_NOTMUCH_STATUS_SUCCESS {
-            Err(NotmuchError::DbFailedToOpen(NotmuchErrorDetail {
-                message: format!("Could not open database: {:?}", message),
-                original_error: result,
-            }))
+            Err(NotmuchError::DbFailedToOpen(convert_error_message(msg)))
         } else {
             Ok(NotmuchDb { db_ptr })
         }
