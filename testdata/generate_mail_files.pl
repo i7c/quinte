4e4310983b5951430ac8c0bff2f1fea9cd2477cd
Constantin Weißer <i7c@posteo.de>
2020-09-11 16:10:51 +0200

    Add script to generate some random mail files in RFC2822 format

---
+++ testdata/generate_mail_files.pl
@@ -0,0 +1,61 @@
+#!/bin/perl
+use Mail::File;
+use strict;
+
+my $mailtext_input = <<EOF
+Alfred's grandfather, Ecgberht, became king of Wessex in 802, and in the view of the historian Richard Abels, it must have seemed very unlikely to contemporaries that he would establish a lasting dynasty. For 200 years, three families had fought for the West Saxon throne, and no son had followed his father as king. No ancestor of Ecgberht had been a king of Wessex since Ceawlin in the late sixth century, but he was believed to be a paternal descendant of Cerdic, the founder of the West Saxon dynasty.[d] This made Ecgberht an ætheling – a prince eligible for the throne. But after Ecgberht's reign, descent from Cerdic was no longer sufficient to make a man an ætheling. When Ecgberht died in 839, he was succeeded by his son Æthelwulf; all subsequent West Saxon kings were descendants of Ecgberht and Ætheluwlf, and were also sons of kings.[17]
+
+At the beginning of the ninth century, England was almost wholly under the control of the Anglo-Saxons. Mercia dominated southern England, but its supremacy came to an end in 825 when it was decisively defeated by Ecgberht at the Battle of Ellendun.[18] The two kingdoms became allies, which was important in the resistance to Viking attacks.[19] In 853, King Burgred of Mercia requested West Saxon help to suppress a Welsh rebellion, and Æthelwulf led a West Saxon contingent in a successful joint campaign. In the same year Burgred married Æthelwulf's daughter, Æthelswith.[20]
+
+In 825, Ecgberht sent Æthelwulf to invade the Mercian sub-kingdom of Kent, and its sub-king, Baldred, was driven out shortly afterwards. By 830, Essex, Surrey and Sussex had submitted to Ecgberht, and he had appointed Æthelwulf to rule the south-eastern territories as king of Kent.[21] The Vikings ravaged the Isle of Sheppey in 835, and the following year they defeated Ecgberht at Carhampton in Somerset,[22] but in 838 he was victorious over an alliance of Cornishmen and Vikings at the Battle of Hingston Down, reducing Cornwall to the status of a client kingdom.[23] When Æthelwulf succeeded, he appointed his eldest son Æthelstan as sub-king of Kent.[24] Ecgberht and Æthelwulf may not have intended a permanent union between Wessex and Kent because they both appointed sons as sub-kings and charters in Wessex were attested (witnessed) by West Saxon magnates, and Kentish charters were witnessed by the Kentish elite; both kings kept overall control and the sub-kings were not allowed to issue their own coinage.[25]
+
+Viking raids increased in the early 840s on both sides of the English Channel, and in 843 Æthelwulf was defeated at Carhampton.[24] In 850, Æthelstan defeated a Danish fleet off Sandwich in the first recorded naval battle in English history.[26] In 851 Æthelwulf and his second son, Æthelbald, defeated the Vikings at the Battle of Aclea and, according to the Anglo-Saxon Chronicle, "there made the greatest slaughter of a heathen raiding-army that we have heard tell of up to this present day, and there took the victory".[27] Æthelwulf died in 858 and was succeeded by his oldest surviving son, Æthelbald, as king of Wessex and by his next oldest son, Æthelberht, as king of Kent. Æthelbald only survived his father by two years and Æthelberht then for the first time united Wessex and Kent into a single kingdom.[28]
+Childhood
+Alfred's father Æthelwulf of Wessex in the early 14th-century Genealogical Roll of the Kings of England
+
+According to Asser, in his childhood Alfred won a beautifully decorated book of English poetry, offered as a prize by his mother to the first of her sons able to memorise it. He must have had it read to him because his mother died when he was about six and he did not learn to read until he was 12.[29] In 853, Alfred is reported by the Anglo-Saxon Chronicle to have been sent to Rome where he was confirmed by Pope Leo IV, who "anointed him as king".[30] Victorian writers later interpreted this as an anticipatory coronation in preparation for his eventual succession to the throne of Wessex. This is unlikely; his succession could not have been foreseen at the time because Alfred had three living elder brothers. A letter of Leo IV shows that Alfred was made a "consul" and a misinterpretation of this investiture, deliberate or accidental, could explain later confusion.[14] It may be based upon the fact that Alfred later accompanied his father on a pilgrimage to Rome where he spent some time at the court of Charles the Bald, king of the Franks, around 854–855.[31] On their return from Rome in 856, Æthelwulf was deposed by his son Æthelbald. With civil war looming, the magnates of the realm met in council to form a compromise. Æthelbald retained the western shires (i.e. historical Wessex), and Æthelwulf ruled in the east. After King Æthelwulf died in 858, Wessex was ruled by three of Alfred's brothers in succession: Æthelbald, Æthelberht and Æthelred.[32]
+The reigns of Alfred's brothers
+A map of the route taken by the Viking Great Heathen Army which arrived in England from Denmark, Norway, and southern Sweden in 865.
+
+Alfred is not mentioned during the short reigns of his older brothers Æthelbald and Æthelberht. The Anglo-Saxon Chronicle describes the Great Heathen Army of Danes landing in East Anglia with the intent of conquering the four kingdoms which constituted Anglo-Saxon England in 865.[33] Alfred's public life began in 865 at age 16 with the accession of his third brother, 18-year-old Æthelred. During this period, Bishop Asser gave Alfred the unique title of secundarius, which may indicate a position similar to the Celtic tanist, a recognised successor closely associated with the reigning monarch. This arrangement may have been sanctioned by Alfred's father or by the Witan to guard against the danger of a disputed succession should Æthelred fall in battle. It was a well known tradition among other Germanic peoples - such as the Swedes and Franks to whom the Anglo-Saxons were closely related - to crown a successor as royal prince and military commander.[34]
+Viking invasion
+
+In 868, Alfred was recorded as fighting beside Æthelred in a failed attempt to keep the Great Heathen Army led by Ivar the Boneless out of the adjoining Kingdom of Mercia.[35] The Danes arrived in his homeland at the end of 870 and nine engagements were fought in the following year, with mixed results; the places and dates of two of these battles have not been recorded. A successful skirmish at the Battle of Englefield in Berkshire on 31 December 870 was followed by a severe defeat at the siege and the Battle of Reading by Ivar's brother Halfdan Ragnarsson on 5 January 871. Four days later, the Anglo-Saxons won a brilliant victory at the Battle of Ashdown on the Berkshire Downs, possibly near Compton or Aldworth. Alfred is particularly credited with the success of this last battle.[34] The Saxons were defeated at the Battle of Basing on 22 January. They were defeated again on 22 March at the Battle of Merton (perhaps Marden in Wiltshire or Martin in Dorset).[34] Æthelred died shortly afterwards in April.[34]
+King at war
+Early struggles
+
+In April 871 King Æthelred died and Alfred acceded to the throne of Wessex and the burden of its defence, even though Æthelred left two under-age sons, Æthelhelm and Æthelwold. This was in accordance with the agreement that Æthelred and Alfred had made earlier that year in an assembly at an unidentified place called Swinbeorg. The brothers had agreed that whichever of them outlived the other would inherit the personal property that King Æthelwulf had left jointly to his sons in his will. The deceased's sons would receive only whatever property and riches their father had settled upon them and whatever additional lands their uncle had acquired. The unstated premise was that the surviving brother would be king. Given the Danish invasion and the youth of his nephews, Alfred's accession probably went uncontested.[36]
+
+While he was busy with the burial ceremonies for his brother, the Danes defeated the Saxon army in his absence at an unnamed spot and then again in his presence at Wilton in May.[34] The defeat at Wilton smashed any remaining hope that Alfred could drive the invaders from his kingdom. Alfred was forced instead to make peace with them, according to sources that do not tell what the terms of the peace were. Bishop Asser claimed that the pagans agreed to vacate the realm and made good their promise.[37]
+
+The Viking army withdrew from Reading in the autumn of 871 to take up winter quarters in Mercian London. Although not mentioned by Asser or by the Anglo-Saxon Chronicle, Alfred probably paid the Vikings cash to leave, much as the Mercians were to do in the following year.[37] Hoards dating to the Viking occupation of London in 871/872 have been excavated at Croydon, Gravesend and Waterloo Bridge. These finds hint at the cost involved in making peace with the Vikings. For the next five years, the Danes occupied other parts of England.[38]
+
+In 876, under their three leaders Guthrum, Oscetel and Anwend, the Danes slipped past the Saxon army and attacked and occupied Wareham in Dorset. Alfred blockaded them but was unable to take Wareham by assault. He negotiated a peace that involved an exchange of hostages and oaths, which the Danes swore on a "holy ring" associated with the worship of Thor. The Danes broke their word, and after killing all the hostages, slipped away under cover of night to Exeter in Devon.[39]
+
+Alfred blockaded the Viking ships in Devon, and with a relief fleet having been scattered by a storm, the Danes were forced to submit. The Danes withdrew to Mercia. In January 878, the Danes made a sudden attack on Chippenham, a royal stronghold in which Alfred had been staying over Christmas "and most of the people they killed, except the King Alfred, and he with a little band made his way by wood and swamp, and after Easter he made a fort at Athelney in the marshes of Somerset, and from that fort kept fighting against the foe".[40] From his fort at Athelney, an island in the marshes near North Petherton, Alfred was able to mount a resistance campaign, rallying the local militias from Somerset, Wiltshire and Hampshire.[34] 878 was the nadir of the history of the Anglo-Saxon kingdoms. With all the other kingdoms having fallen to the Vikings, Wessex alone was resisting.[41] 
+EOF
+;
+my @words = split /[,;\s]+/, $mailtext_input;
+my $pos = 0;
+
+sub give_words {
+  my ($amount) = @_;
+
+  $pos = ($pos + $amount) % (scalar @words - 100);
+  join " ", @words[$pos .. $pos + $amount - 1]
+}
+
+my @senders = ("hodor", "peter", "gandalf", "harry", "superman", "gibbs");
+
+for my $i (1 .. 128) {
+  my %hash = (
+    From       => $senders[$i % scalar @senders] . '@example.com',
+    To         => 'you@example.com',
+    Subject    => give_words(7) . "!",
+    Body       => give_words(50),
+    template   => 'mail-XXXX.tmp'
+  );
+
+  my $mail = Mail::File->new(%hash);
+  $mail->send;
+}
